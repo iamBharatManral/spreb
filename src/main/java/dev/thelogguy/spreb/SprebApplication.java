f0437d99a0547dc74855591451a8311f8390f781
iamBharatManral <justbharatmanral@gmail.com>
2024-07-18 14:55:59 +0530

    init: initialize the project with welcome message

---
+++ src/main/java/dev/thelogguy/spreb/SprebApplication.java
@@ -0,0 +1,13 @@
+package dev.thelogguy.spreb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprebApplication.class, args);
+    }
+
+}
---
+++ src/main/java/dev/thelogguy/spreb/controllers/MainController.java
@@ -0,0 +1,12 @@
+package dev.thelogguy.spreb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+   @GetMapping("/home")
+   public String home() {
+       return "home.html";
+   }
+}
